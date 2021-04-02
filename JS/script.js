230c8d215f84d1bfe65c32d112c3a39cda912c68
Cruz <ulisescrz.16@gmail.com>
2021-04-02 10:18:12 -0600

    agregue estilos y animacion del texto idiomas

--- JS/script.js
+++ JS/script.js
@@ -20,4 +20,22 @@ btnTooggle.addEventListener('click', function (){
     }
 })
 
-//inicializacion de la libreria de animaciones
+//inicializacion de la libreria de animaciones (esta es para texto)
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="idioma">Francés</i>',
+        '<i class="idioma">Italiano</i>',
+        '<i class="idioma">Portugués</i>'
+    ],
+    stringsElement: '#cadenas-texto',//ID del elemento que contiene cadenasde texto a mostrar
+    typeSpeed:75,//velocidad en milisegundos para poner una letra.
+    startDelay: 300,//Tiempo de retraso  en iniciar la animacion. Aplica tambien cuanto termina y vuelve a iniciar.
+    smartBackspace: true,//eliminar solamente las palabras que sean nuevas en una cadena de texto.
+    shuffle: false,//Alterar el orden en el que se escribe las palabras.
+    backDelay: 1500,//Tiempo de espera despues de que termine de escribir una palabra.
+    loop: true,//repetir el array de strings
+    loopCount:false,//cantidad de veces a repetir el array. false = infinito.
+    showCursos:true,//mostrar cursor palpitando.
+    cursorChar: '|',//caracter para el cursor.
+    contentType: 'html',//'html' o 'null' para textos sin formato
+});
