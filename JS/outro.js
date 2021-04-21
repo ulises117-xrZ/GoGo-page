b9afe5ceb66854609711e21d98e216a4041288b1
Ulises <rollercruz60@gmail.com>
2021-04-20 20:35:10 -0600

    seccion de practica

--- JS/outro.js
+++ JS/outro.js
@@ -11,3 +11,48 @@ listening.addEventListener('click', error);
 function error(){
     alert("Esta sección estara lista proximamente!")
 }
+
+const acordionHeader = document.querySelectorAll(".accordion-item-header");
+const footer = document.querySelector(".footer");
+
+acordionHeader.forEach(acordionHeader =>{
+    acordionHeader.addEventListener('click', evt =>{
+
+        const currentActive = document.querySelector('.accordion-item-header.active');
+        if(currentActive && currentActive !== acordionHeader){
+            currentActive.classList.toggle('active');
+            currentActive.nextElementSibling.style.maxHeight = 0;
+        } 
+
+        acordionHeader.classList.toggle('active');
+        const accordionItemBody = acordionHeader.nextElementSibling;
+        if(acordionHeader.classList.contains('active')){
+            accordionItemBody.style.maxHeight = "15000px";
+        } else{
+            accordionItemBody.style.maxHeight = 0;
+        }
+    })
+})
+
+
+const accordionItemHeader = document.querySelectorAll(".accordion-item-heade");
+accordionItemHeader.forEach(accordionItemHeader =>{
+    accordionItemHeader.addEventListener('click', evt =>{
+        const currentActive2 = document.querySelector('.accordion-item-heade.activate');
+        if(currentActive2 && currentActive2 !== accordionItemHeader){
+            currentActive2.classList.toggle('activate');
+            currentActive2.nextElementSibling.style.maxHeight = 0;
+        } 
+
+        accordionItemHeader.classList.toggle('activate');
+        
+        footer.classList.add('visible');
+        const accordionItemBody2 = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains('activate')){
+            accordionItemBody2.style.maxHeight = "15000px";
+        } else{
+            accordionItemBody2.style.maxHeight = 0;
+            footer.classList.remove('visible');
+        }
+    })
+})
